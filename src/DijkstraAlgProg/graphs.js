688aafc98c6de3aca45e268cf63389931be2ecd9
efgefg0001 <efg.efg.0001@gmail.com>
2015-10-06 01:57:36 +0300

    Implemented function for Edge constructor

--- src/DijkstraAlgProg/graphs.js
+++ src/DijkstraAlgProg/graphs.js
@@ -2,3 +2,9 @@ function Vertex(__id, __data) {
     this.id = function () { return __id; }
     this.data = function () { return __data; }
 }
+
+function DirectedWeightedEdge(__from, __to, __weight) {
+    this.from = function () { return __from; }
+    this.to = function () { return __to; }
+    this.weight = function () { return __weight; }
+}
