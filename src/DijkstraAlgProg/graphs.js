0205095ed02208366d6cded2f4e589b93db2bf25
efgefg0001 <efg.efg.0001@gmail.com>
2015-10-06 04:16:22 +0300

    Methods for removing

--- src/DijkstraAlgProg/graphs.js
+++ src/DijkstraAlgProg/graphs.js
@@ -9,6 +9,10 @@ function DirectedWeightedEdge(__from, __to, __weight) {
     this.weight = function () { return __weight; }
 }
 
+function VertexInArrayError(vertex) {
+    this.message = "";
+}
+
 function EdgeWeightedDigraph() {
     var __vertices = [];
     var __edges = {};
@@ -17,11 +21,28 @@ function EdgeWeightedDigraph() {
     this.edges = function () { return __edges; }
 
     this.addVertex = function (vertex) {
+        if (__vertices.indexOf(vertex.id) !== -1)
+            throw "Vertex is in array";
         __vertices.push(vertex);
         __edges[vertex.id] = [];
     }
 
     this.addEdge = function (edge) {
         __edges[edge.from()].push(edge);
+    }
+
+    this.removeVertex = function (id) {
+        var i = 0, len = __vertices.length;
+        while (i < len)
+            if (__vertices[i].id === id) {
+                __vertices.splice(i, 1);
+                len = __vertices.length;
+            } else
+                ++i;
+        delete __edges[id];
+    }
+
+    this.removeEdge = function (edge) {
+        throw "Not implemented";
     }
 }
